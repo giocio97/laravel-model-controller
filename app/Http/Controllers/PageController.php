57a6373982f190e7eedbcd492e02d2247d5f2514
Giovanni Ciotta <giociotta@gmail.com>
2022-07-14 17:59:19 +0200

    first commit

---
+++ app/Http/Controllers/PageController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Movie;
+use Illuminate\Http\Request;
+
+class PageController extends Controller
+{
+    public function list (){
+       $film = Movie::all();
+       dd($film);
+    }
+}
